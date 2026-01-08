9a36571051c28e9190a45d80a0de74e00b9e7991
Songlin Jiang <clorf6@sjtu.edu.cn>
2026-01-09 02:22:35 +0800

    Remove unused directory

--- src/boltz/data/evaluation/utils.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def get_subdirectories(directory_path):
-    # Get all sub dir.
-    subdirectories = [d for d in os.listdir(directory_path) if os.path.isdir(os.path.join(directory_path, d))]
-    return subdirectories
